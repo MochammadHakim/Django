44f53b5a15289f6d13508e26bdf17c9579a01ac0
Putri1106 <sandrinaekaputri116@gmail.com>
2024-05-12 17:04:03 +0700

    sandrina

---
+++ mywebsite/library/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request):
+    return render(request, 'base.html')
+
+def input(request):
+    return render(request, 'input.html')
