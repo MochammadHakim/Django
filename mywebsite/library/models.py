e4a4a8f8e95f45ecb391f1e53836aab4876ff616
Jospeh <134275356+ItsRon23@users.noreply.github.com>
2024-05-16 09:59:39 +0700

    initialize database

--- mywebsite/library/models.py
+++ mywebsite/library/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class BookAttribute(models.Model):
+    judulBuku = models.CharField(max_length=120)
+    tahunTerbit = models.IntegerField(null=False, default=0)
+    penulisBuku = models.CharField(max_length=120, null=True, blank=True)
+    kategoriBuku = models.CharField(max_length=120, null=True, blank=True)
+    jumlahHalaman = models.IntegerField(null=False, default=0)
+    coverBuku = models.ImageField(null=False, blank=True, upload_to=None, height_field=None, width_field=None, max_length=100)
+
+    def __str__(self):
+        return self.judulBuku
--- mywebsite/library/views.py
+++ mywebsite/library/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import BookAttribute
 
 # Create your views here.
 def home(request):
-    return render(request, 'base.html')
+    all_books = BookAttribute.objects.all
+    return render(request, 'base.html', {'all':all_books})
+    # return render(request, 'base.html')
+
+# def LibraryList(request):
+#     all_books = LibraryList.objects.all
+#     return render(request, 'base.html', {'all':all_books})
+#     # return render(request, 'base.html')
