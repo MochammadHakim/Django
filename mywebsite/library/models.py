671d48398833dfc3d34ea0bfded2ce62cd3f5794
Louis Mamusung <lzmamusung@Louiss-MacBook-Air.local>
2024-05-17 20:38:01 +0700

    add attribute of book

--- mywebsite/library/models.py
+++ mywebsite/library/models.py
@@ -3,12 +3,16 @@ from django.db import models
 # Create your models here.
 
 class BookAttribute(models.Model):
-    judulBuku = models.CharField(max_length=120)
-    tahunTerbit = models.IntegerField(null=False, default=0)
-    penulisBuku = models.CharField(max_length=120, null=True, blank=True)
-    kategoriBuku = models.CharField(max_length=120, null=True, blank=True)
-    jumlahHalaman = models.IntegerField(null=False, default=0)
-    coverBuku = models.ImageField(null=False, blank=True, upload_to=None, height_field=None, width_field=None, max_length=100)
-
+    judulBuku = models.CharField(max_length=255)
+    penulisBuku = models.CharField(max_length=255, blank=True, null=False)
+    # --- FORMAT TAHUN TERBIT = YYYY-MM-DD
+    tahunTerbit = models.DateField()
+    kategoriBuku = models.CharField(max_length=255, blank=True, null=False)
+    jumlahHalaman = models.IntegerField()
+    coverBuku = models.ImageField(upload_to='covers/')
+    status_peminjaman = models.CharField(max_length=20, null=False, blank=True, choices=[
+        ('available', 'Available'),
+        ('not-available', 'Not Available'),
+    ])
     def __str__(self):
         return self.judulBuku
