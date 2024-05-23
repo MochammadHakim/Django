064faa444758e1e673b2afa48b13d8ad6a5fa417
Jospeh <134275356+ItsRon23@users.noreply.github.com>
2024-05-23 13:23:08 +0700

    add urls.py in library

---
+++ mywebsite/library/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.book_list, name='book_list'),
+    path('book/<int:pk>/', views.book_detail, name='book_detail'),
+    path('book/new/', views.book_create, name='book_create'),
+    path('book/<int:pk>/delete/', views.book_delete, name='book_delete'),
+]
